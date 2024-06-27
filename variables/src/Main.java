3fc912fd1fcb03355f1ddfb4be67de6e8882d115
Mete Solmaz <metesolmaz3450@gmail.com>
2024-06-28 01:43:21 +0300

    added examples for basic java data types and variables.

---
+++ variables/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ogrenciSayisi = 11;  // Degisken tanımlama
+        String mesaj = "Ogrenci sayisi : ";
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("Öğrenci sayisi : " + ogrenciSayisi);
+        System.out.println("Öğrenci sayisi : " + ogrenciSayisi);
+    }
+}
