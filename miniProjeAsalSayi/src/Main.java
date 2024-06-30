d755c46b5175428819f2685c6552fd64c360ddf0
Mete Solmaz <metesolmaz3450@gmail.com>
2024-06-30 22:59:36 +0300

    Added examples for strings and some mini projects.

---
+++ miniProjeAsalSayi/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = -5;
+        boolean isPrime = true;
+
+        if(number==1){
+            System.out.println("Sayı asal degildir.");
+            return;
+        }
+
+        if(number<1){
+            System.out.println("Geçersiz sayı...");
+        }
+
+        for(int i=2;i<number;i++){
+            if(number%i==0){
+                isPrime = false;
+            }
+        }
+        if(isPrime){
+            System.out.println("Sayı asaldır.");
+        }
+        else{
+            System.out.println("Sayı asal değildir.");
+        }
+    }
+}
