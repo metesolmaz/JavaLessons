32de18981f10fbbdd5df943b4d13f8ba3d32a014
Mete Solmaz <metesolmaz3450@gmail.com>
2024-07-03 03:12:01 +0300

    Added examples for generics and a demo.

---
+++ generics/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        MyList<Customer> sehirler = new MyList<Customer>();
+        sehirler.add(new Customer());
+        sehirler.add(new Customer());
+    }
+}
